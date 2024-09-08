46b2b432004bd9658a5c742cfc5e6f7b00d61c83
Thomas Kekeisen <bitbucket@tk.ca.kekeisen.it>
2024-09-08 12:57:48 +0200

    Image width and size is now a parameter, removed comments, added encoding to .mov, removed color change (https://lulububu.atlassian.net/browse/BASTELVIDEOS-255)

--- stl2gif.sh
+++ stl2gif.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 set -ue
+
+# Variables for image size
+IMG_WIDTH=1920
+IMG_HEIGHT=1080
+
 if [ -z "$(docker --version)" ]; then
 	echo "Docker is not installed. Please install docker before running this script."
 	exit 1
 fi
+
 docker pull spuder/stl2origin:latest
 docker pull linuxserver/ffmpeg:version-4.4-cli
 docker pull openscad/openscad:2021.01
@@ -27,7 +33,6 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
     echo "Copying ${filename}.stl to stl2gif-input docker volume"
 	docker cp "${dirname}/${filename}.stl" "${INPUT_ID}:/input/${filename}.stl"
 
-    # Detect how offcenter STL file is. Save variable output to foo.sh ($XTRANS, $YTRANS, $ZTRANS)
     echo ""
     echo "Detecting ${filename} offset from origin"
     echo "========================================"
@@ -42,10 +47,8 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
 
 	docker cp "${OUTPUT_ID}:/output/foo.sh" "${MYTMPDIR}/foo.sh"
 
-    # cp "${file}" "$MYTMPDIR/foo.stl"
     source $MYTMPDIR/foo.sh
     cat ${MYTMPDIR}/foo.sh
-    # cat $MYTMPDIR/foo.sh
 
     echo ""
     echo "Duplicating ${filename} and centering object at origin"
@@ -56,11 +59,7 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
         -v stl2gif-output:/output \
         openscad/openscad:2021.01 openscad /dev/null -D "translate([$XTRANS-$XMID,$YTRANS-$YMID,$ZTRANS-$ZMID])import(\"/input/${filename}.stl\");" -o "/output/foo-centered.stl"
     docker cp "${OUTPUT_ID}:/output/foo-centered.stl" "${MYTMPDIR}/foo-centered.stl"
-    # Take temporary (centered) stl and create collection of .png images to be animated
-    # TODO: move to docker once opengl issue is solved
-    # https://github.com/openscad/openscad/issues/4028
-    # This solution is very fast, but is not portable. 
-    # Check if openscad is in path
+
     echo ""
     echo "Converting ${filename} into 360 degree .png files"
     echo "=================================================="
@@ -73,53 +72,32 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
         echo "OpenSCAD is not installed. Please install openscad before running this script."
         exit 1
     fi
-    # Check openscad version is 2020.01 or later
+
     openscad_version=$($openscad_path -v 2>&1 | grep -o '\d\d\d\d')
     if [ "$openscad_version" -lt "2021" ]; then
         echo "OpenSCAD 2021.01 or later is required to run this script. Please update openscad before running this script."
         exit 1
     fi
-    # Check hsvtorgb.scad is in openscad library path
-    #TODO: it would be better to use a relative path `use <lib/hsvtorgb.scad>``
-    # howevever that isn't working for some reason
+
     if [ ! -f "$HOME/Documents/OpenSCAD/libraries/hsvtorgb.scad" ]; then
         echo "hsvtorgb.scad is not installed. Installing"
-        # copy hsvtorgb.scad to openscad library path
         mkdir -p "$HOME/Documents/OpenSCAD/libraries"
         cp lib/hsvtorgb.scad "$HOME/Documents/OpenSCAD/libraries/hsvtorgb.scad"
     fi
 
     $openscad_path /dev/null \
         -D 'use <hsvtorgb.scad>;' \
-        -D '$vpr = [60, 0, 360 * $t];' \
+        -D '$vpr = [0, 60, 360 * $t];' \
         -o "${MYTMPDIR}/foo.png"  \
-        -D "color(hsvToRGB(\$t,1,1)) import(\"${MYTMPDIR}/foo-centered.stl\");" \
-        --imgsize=600,600 \
-        --animate 60 \
-        --viewall --autocenter \
+        -D "color([68/255, 127/255, 244/255]) import(\"${MYTMPDIR}/foo-centered.stl\");" \
+        --imgsize=${IMG_WIDTH},${IMG_HEIGHT} \
+        --camera '0,0,0,0,0,360 * $t,10' \
+        --animate 120 \
+        --viewall \
+        --autocenter \
         --preview \
         --colorscheme "Tomorrow Night" \
         --quiet
-
-    # This solution works but is very slow and has some glitches
-    # for ((angle=0; angle <=360; angle+=5)); do
-    #     echo "Rendering $angle"
-    #     ls $MYTMPDIR
-    #     # openscad /dev/null -o dump$angle.png  -D "cube([2,3,4]);" --imgsize=250,250 --camera=0,0,0,45,0,$angle,25
-    #     docker run \
-    #         -v "$MYTMPDIR:/input" \
-    #         -v "$MYTMPDIR:/output" \
-    #         --init \
-    #         openscad/openscad:2021.01 xvfb-run -a openscad /dev/null -D "\$vpr = [60, 0, ${angle}];" -D 'import("/input/foo-centered.stl");' -o "/output/bar${angle}.png" --imgsize=600,600 --autocenter --viewall --quiet
-    # done
-
-    # This solution crashes after 4 annimations. 40+ hours of debugging and I haven't found a solution yet
-    # https://github.com/openscad/openscad/issues/4028
-    # docker run --init --gpus all \
-	# 	-v "${dirname}:/input" \
-	# 	-v "${MYTMPDIR}:/output" \
-	# 	openscad/openscad:2021.01 xvfb-run -a openscad "/input/foo.scad" -o "/output/foo.png" --animate 60
-    # ls $MYTMPDIR
     
     echo ""
     echo "Copying .png files to docker volume"
@@ -128,13 +106,24 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
     done
 
     echo ""
-    echo "Converting ${filename} .PNG files into .GIF"
+    echo "Converting ${filename} .PNG files into .GIF and rotating 90° to the right"
     echo "==========================================="
     docker run --rm \
         -v stl2gif-input:/input \
         -v stl2gif-output:/output \
-        linuxserver/ffmpeg:version-4.4-cli -y -framerate 15 -pattern_type glob -i 'input/*.png' -r 25 -vf scale=512:-1 "/output/${filename}.gif";
-    docker cp "${OUTPUT_ID}:/output/${filename}.gif" "${dirname}/${filename}.gif"
+        linuxserver/ffmpeg:version-4.4-cli -y -framerate 15 -pattern_type glob -i 'input/*.png' -r 25 -vf "scale=2024:-1,transpose=1" "/output/${filename}.gif";
+    # docker cp "${OUTPUT_ID}:/output/${filename}.gif" "${dirname}/${filename}.gif"
+
+    echo ""
+    echo "Converting ${filename} .GIF to .MOV"
+    echo "==================================="
+    docker run --rm \
+        -v stl2gif-input:/input \
+        -v stl2gif-output:/output \
+        linuxserver/ffmpeg:version-4.4-cli -y -i "/output/${filename}.gif" -movflags faststart -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -pix_fmt yuv420p "/output/${filename}.mov";
+    docker cp "${OUTPUT_ID}:/output/${filename}.mov" "${dirname}/${filename}.mov"
+
+
     ls "${MYTMPDIR}"
     echo ""
     echo "Cleaning up temp directory ${MYTMPDIR}"
